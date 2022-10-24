3cff1770c4c98a9e6e9f1cab924de90a3d4fa3f8
Keshan Moosai <keshanmoosai@gmail.com>
2022-10-24 13:50:42 -0400

    future proofing

--- burnAttendance.js
+++ burnAttendance.js
@@ -13,6 +13,7 @@
     'use strict';
     let calendarObjects = [];
     let attendanceIconURL = `https://myelearning.sta.uwi.edu/theme/image.php/boost/attendance/`
+    let rmClass = "calendar_event_course"; // Class to remove from the calendar objects
 
     async function getResults() {
         console.log("Fetching all calendar objects that contain events");
@@ -22,18 +23,18 @@
 
     async function burnTheShits() {
         for (let one in calendarObjects) {
-            let currDay = calendarObjects[one];
-            let numEvents = String(currDay.outerText).split(",")[0].trim();
-            let currDayTitle = String(currDay.outerText).split("\n")[0].substring(numEvents.length + 2);
+            let currDayObj = calendarObjects[one];
+            let numEvents = String(currDayObj.outerText).split(",")[0].trim();
+            let currDayObjTitle = String(currDayObj.outerText).split("\n")[0].substring(numEvents.length + 2);
 
-            // This is probably the one with the attendance bullshit
-            let numOccurences = String(currDay.outerHTML).split(attendanceIconURL).length - 1;
+            // Count the number of occurence of the 'Attendance' event (denoted by the icon URL above)
+            let numOccurences = String(currDayObj.outerHTML).split(attendanceIconURL).length - 1;
 
-            // If matches equivalent to number of event for that day, BURN IT
+            // If occurences match the number of events for that day, remove class from its calendar object
             if (Number(numEvents.split(" ")[0]) == Number(numOccurences)) {
-                let origVal = String(currDay.attributes[0].nodeValue);
-                currDay.attributes[0].nodeValue = origVal.split("calendar_event_course").join(" ");
-                console.log(`BURNT -> ${currDayTitle}`);
+                let origVal = String(currDayObj.attributes[0].nodeValue);
+                currDayObj.attributes[0].nodeValue = origVal.split(rmClass).join(" ");
+                console.log(`BURNT -> ${currDayObjTitle}`);
             }
         }
     }
