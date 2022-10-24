fc21a0ecf8267d789216f96c0a3259c3c7cf8018
Keshan Moosai <keshanmoosai@gmail.com>
2022-10-24 14:05:44 -0400

    restructured burnAttendance.js code

--- burnAttendance.js
+++ burnAttendance.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Burn The Attendance
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Burns all the damn annyoing stupid ridiculous attendance notices on myeLearning!
 // @author       MadMoose02
 // @match        https://myelearning.sta.uwi.edu/*
@@ -13,15 +13,21 @@
     'use strict';
     let calendarObjects = [];
     let attendanceIconURL = `https://myelearning.sta.uwi.edu/theme/image.php/boost/attendance/`
+    let findString = "td.hasevent";
     let rmClass = "calendar_event_course"; // Class to remove from the calendar objects
 
-    async function getResults() {
+    function getResults() {
         console.log("Fetching all calendar objects that contain events");
-        calendarObjects = Array.from(document.querySelectorAll(`td.hasevent`));
-        console.log("Done");
+        try { 
+            calendarObjects = Array.from(document.querySelectorAll(findString)); 
+            return true
+        } catch (e) { 
+            console.log("Could not fetch calendar objects. Error details below.\n", e);
+            return false
+        }
     }
 
-    async function burnTheShits() {
+    function burnTheShits() {
         for (let one in calendarObjects) {
             let currDayObj = calendarObjects[one];
             let numEvents = String(currDayObj.outerText).split(",")[0].trim();
@@ -33,13 +39,16 @@
             // If occurences match the number of events for that day, remove class from its calendar object
             if (Number(numEvents.split(" ")[0]) == Number(numOccurences)) {
                 let origVal = String(currDayObj.attributes[0].nodeValue);
+
+                // Re-assign class values without 'rmClass' value
                 currDayObj.attributes[0].nodeValue = origVal.split(rmClass).join(" ");
                 console.log(`BURNT -> ${currDayObjTitle}`);
             }
         }
     }
 
-    setTimeout(getResults, 2499);
-    setTimeout(burnTheShits, 2600);
+    setTimeout(function() {
+        (getResults() === true) ? burnTheShits() : console.log("Could not burn the attendance!");
+    }, 2499);
 
 })();
