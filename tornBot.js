4181642f25e103dd5b5b3774e69176c0f4052b9d
Keshan Moosai <keshanmoosai@gmail.com>
2023-03-01 22:08:45 -0400

    Added object data for bruteforcing verification

---
+++ tornBot.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         TornBot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.torn.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const singleDigits = {
+        0 : "zero",
+        1 : "one",
+        2 : "two",
+        3 : "three",
+        4 : "four",
+        5 : "five",
+        6 : "six",
+        7 : "seven",
+        8 : "eight",
+        9 : "nine"
+    };
+
+    const doubleDigits = {
+        10 : "ten",
+        11 : "eleven",
+        12 : "twelve",
+        13 : "thirteen",
+        14 : "fourteen",
+        15 : "fifteen",
+        16 : "sixteen",
+        17 : "seventeen",
+        18 : "eighteen",
+        19 : "nineteen",
+        20 : "twenty",
+        30 : "thirty",
+        40 : "forty",
+        50 : "fifty",
+        60 : "sixty",
+        70 : "seventy",
+        80 : "eighty",
+        90 : "ninty"
+    };
+
+    function integerToWordEquiv(num) {
+        
+    };
+
+    // Only start when detected a url match
+    let url = `https://www.torn.com/index.php`;
+    if (window.location.href === url) {
+        doGym();
+        doCrime();
+    }
+})();
