76fafae9d3f9cb7ce3df8140f2c752993dfdcf6b
Moose <keshanmoosai@gmail.com>
2023-10-22 17:48:47 -0400

    Updated Microsoft auto-login procedure

--- tornAuto.js
+++ tornAuto.js
@@ -1,34 +1,48 @@
 // ==UserScript==
-// @name         Torn-AutoLogin
+// @name         Microsoft-AutoLogin
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.torn.com/
-// @icon         https://www.torn.com/images/v2/torn_logo/regular/desktop/logo@1x.webp
+// @description  Automatically log-in with School Account when logging on to myeLearning
+// @author       MadMoose02
+// @match        https://login.microsoftonline.com/*
+// @icon         https://icons.duckduckgo.com/ip2/uwi.edu.ico
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    
-    // Look for the buttin with class 'login' in it
-    function login() {
-        let x = document.querySelectorAll('button');
-        for (let curr in x) {
-            let y = (x[curr].attributes ? x[curr].attributes[0].textContent : "");
-            if (String(y).includes('login')) {
-                console.log('Detected login button on page!');
 
-                // Click the button after locating it and sign in to the website
-                x[curr].click();
-                x = document.querySelector("input[name='btnLogin']")
-                console.log('Logging-in to Torn ... ');
-                x.click();
+    let myEmail = "keshan.moosai@my.uwi.edu"; // Enter my.uwi.edu email here
+
+    // Check if need to navigate the Microsoft user login page
+    function handleMicrosoft() {
+        let userAccounts = Array.from(document.querySelectorAll(".tile-container"));
+        for (let one in userAccounts) {
+            let currUserObj = userAccounts[one].children[0].children[0]
+            let displayName = String(currUserObj.outerText).substring(2).trim();
+            if (displayName !== myEmail) {
+                console.log(`No match found for ${myEmail}!`);
+                return;
             }
+
+            // Click on the tile that contains 'myEmail' in it
+            currUserObj.click();
+            console.log("Fired a click on school email element");
+
+            // After timeout, click the sign-in button by id and click it
+            for (let i = 0; i < 2; i++) {
+                setTimeout(function() {
+                    let signinBtn = document.getElementById("idSIButton9");
+                    signinBtn.click();
+                    console.log(`Fired a click on "${signinBtn.outerText}"`);
+                }, 500)
+            }
+            
+            // Reload webpage. Fail safe in the event prompted to remember login
+            location.reload();
         }
     }
 
-    // Function calls here
-    login();
+    setTimeout(handleMicrosoft, 2000);
+
 })();
